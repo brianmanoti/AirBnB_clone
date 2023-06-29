8c7ec71e9de1ccfe59eda9dd42b798e674b6c0b9
Brianmanoti <Brianmanoti254@gmail.com>
2023-06-29 06:02:10 -0700

    updated

--- console.py
+++ console.py
@@ -1,21 +1,16 @@
-"""console modules"""
+#!/usr/bin/python3
+""" Console Module """
 import cmd
-
 class HBNBCommand(cmd.Cmd):
-    """functionality of the HBNB console"""
+    """ Contains the functionality for the HBNB console"""
     prompt = '(hbnb) '
-
-    def do_quit(self, args):
-        """Exit the program."""
-        return True
-
-    def do_EOF(self, args):
-        """Exit the program."""
-        return True
-
-    def emptyline(self):
-        """Do nothing when an empty line is entered."""
-        pass
+     def do_quit(self, command):
+        """ Method to exit the HBNB console"""
+        exit()
+    def do_EOF(self, arg):
+        """ Handles EOF to exit program """
+        print()
+        exit()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
