1f2401e53da85441966012be0e899f4cbc1f5b2a
brianmanoti <brianmanoti254@gmail.com>
2023-09-23 07:46:44 -0700

    Done fixing Black lines

--- models/base_model.py
+++ models/base_model.py
@@ -19,6 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
