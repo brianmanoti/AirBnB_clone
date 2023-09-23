dad3344c382e174438c8c5f6517ca3fd374a7416
brianmanoti <brianmanoti254@gmail.com>
2023-09-23 11:30:20 -0700

    writting Test cases for the console

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,61 +1,43 @@
 #!/usr/bin/python3
-"""Tests the basemodels"""
+""" Defines unnittests for BaseModel class """
 import unittest
 import os
-import pep8
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+import models
 
 
 class TestBaseModel(unittest.TestCase):
-    """tests for basemodel"""
-    @classmethod
-    def setUpClass(cls):
-        """sets up"""
-        cls.testBase = BaseModel()
-        cls.testBase.x = "x"
-        cls.testBase.y = 100
+    """ Unittests suite for the BaseModel class """
 
-    @classmethod
-    def tearDownClass(cls):
-        """
-        tears down
-        """
-        del cls.testBase
-        try:
-            os.remove("file.json")
-        except:
-            pass
+    def test_new_instance_stored(self):
+        self.assertIn(BaseModel(), models.storage.all().values())
 
-    def test_pep8_basemodel(self):
-        """
-        tests pep8
-        """
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/base_model.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_created_at(self):
+        self.assertEqual(datetime, type(BaseModel().created_at))
 
-    def test_check_functions(self):
-        self.assertIsNotNone(BaseModel.__doc__)
-        self.assertIsNotNone(BaseModel.save.__doc__)
-        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+    def test_updated_at(self):
+        self.assertEqual(datetime, type(BaseModel().updated_at))
 
-    def test_attribute_basemodel(self):
+    def test_init_method(self):
         self.assertTrue(hasattr(BaseModel, "__init__"))
-        self.assertTrue(hasattr(BaseModel, "save"))
-        self.assertTrue(hasattr(BaseModel, "to_dict"))
-
-    def test_init(self):
-        self.assertTrue(isinstance(self.testBase, BaseModel))
 
     def test_save(self):
-        self.testBase.save()
-        self.assertNotEqual(self.testBase.created_at, self.testBase.updated_at)
+        self.assertTrue(hasattr(BaseModel, "save"))
 
     def test_to_dict(self):
-        copy = self.testBase.to_dict()
-        self.assertEqual(self.testBase.__class__.__name__, 'BaseModel')
-        self.assertIsInstance(copy['created_at'], str)
-        self.assertIsInstance(copy['updated_at'], str)
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+
+    def test_id(self):
+        self.assertEqual(str, type(BaseModel().id))
+
+    def test_docstring(self):
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
 
 if __name__ == "__main__":
